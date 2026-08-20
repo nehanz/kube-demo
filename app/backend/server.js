28027cff03c0b7262db4f3ac258c488bff75eff8
Nehan Wijagunarathna <nehannimsara0031029@gmail.com>
2026-08-21 00:17:11 +0530

    feat: add monitoring with Prometheus and Grafana

--- app/backend/server.js
+++ app/backend/server.js
@@ -7,6 +7,33 @@ const DB_NAME = 'demo';
 
 app.use(express.json());
 
+const promClient = require('prom-client');
+
+// Enable default metrics (CPU, Memory, Event Loop)
+promClient.collectDefaultMetrics();
+
+// Custom metric: HTTP Request Duration
+const httpRequestDurationMicroseconds = new promClient.Histogram({
+  name: 'http_request_duration_ms',
+  help: 'Duration of HTTP requests in ms',
+  labelNames: ['method', 'route', 'code'],
+  buckets: [50, 100, 200, 300, 400, 500, 750, 1000, 2000]
+});
+
+// Middleware to track duration of all requests
+app.use((req, res, next) => {
+  const end = httpRequestDurationMicroseconds.startTimer();
+  res.on('finish', () => {
+    end({ route: req.route ? req.route.path : req.path, code: res.statusCode, method: req.method });
+  });
+  next();
+});
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', promClient.register.contentType);
+  res.end(await promClient.register.metrics());
+});
+
 app.get('/health', (req, res) => {
   res.status(200).send('OK');
 });
